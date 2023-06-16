c1400215eddce9a36d1d67c346ac52e19a849e5a
Shlok D. Mange <shlok_mange@protonmail.com>
2023-06-16 09:46:48 +0530

    query client provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
 import '@fontsource/jura'
 import '@fontsource/manrope'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  
+  const queryClient = new QueryClient()
+  
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  )
 }
