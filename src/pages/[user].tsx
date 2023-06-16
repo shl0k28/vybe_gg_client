020aefed02c586b968c34ff5d18524eb95f43756
Shlok D. Mange <shlok_mange@protonmail.com>
2023-06-16 17:08:38 +0800

    user transactions query

--- src/pages/[user].tsx
+++ src/pages/[user].tsx
@@ -17,6 +17,7 @@ const WalletInfo: NextPage = () => {
     const [portfolioData, setPortfolioData] = useState<Array<number>>([])
     const [nftBalances, setNftBalances] = useState<any>()
     const [tokenBalances, setTokenBalances] = useState<any>()
+    const [userTransactions, setUserTransactions] = useState<any>()
 
     // get portfolio data
     const getHistoricalPortfolio = async (chainName: string, address: string) => {
@@ -48,6 +49,15 @@ const WalletInfo: NextPage = () => {
         return data
     }
 
+    const getUserTransactions = async (chainName: string, address: string) => {
+        let url = `http://localhost:8080/api/fetch/transactions?chainName=${chainName}&address=${address}`
+        const res = await fetch(url, { method: 'GET' })
+        const data = await res.json()
+        setUserTransactions(data)
+        console.log(data)
+        return data
+    }
+
     const historicalPortfolioQuery = useQuery({ queryKey: ['history'], 
         queryFn: () => getHistoricalPortfolio('eth-mainnet', '0xCF1C64Ac9075D0a41Bb3e7D5A08E8CCAc512b1d0')
     })
@@ -60,8 +70,25 @@ const WalletInfo: NextPage = () => {
         queryFn: () => getTokenBalances('eth-mainnet', '0xCF1C64Ac9075D0a41Bb3e7D5A08E8CCAc512b1d0')    
     })
 
+    const userTransactionsQuery = useQuery({ queryKey: ['userTransactions'],
+        queryFn: () => getUserTransactions('eth-mainnet', '0xCF1C64Ac9075D0a41Bb3e7D5A08E8CCAc512b1d0')
+    })
+    
     const header = `vybe.gg`
     
+    const backgroundColor = [
+        'rgba(255, 99, 132, 0.2)',   // red
+        'rgba(54, 162, 235, 0.2)',   // blue
+        'rgba(255, 206, 86, 0.2)',   // yellow
+        'rgba(75, 192, 192, 0.2)',   // teal
+        'rgba(153, 102, 255, 0.2)',  // purple
+        'rgba(255, 159, 64, 0.2)',   // orange
+        'rgba(255, 99, 255, 0.2)',   // magenta
+        'rgba(255, 255, 99, 0.2)',   // light yellow
+        'rgba(99, 255, 132, 0.2)',   // light green
+        'rgba(99, 132, 255, 0.2)',   // light blue
+    ]
+
     function getLast30Days() {
         const dates = Array.from({length: 30}, (_, i) => {
             const d = new Date();
@@ -93,16 +120,14 @@ const WalletInfo: NextPage = () => {
         },
     };
 
+    /*
+    
     const chartData = {
         labels: ['USD', "ETH"],
         datasets: [{
           label: 'Token Balances(%)',
           data: tokenBalances.percentagesArray,
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            // Add more colors if you have more cryptocurrencies.
-          ],
+          backgroundColor: backgroundColor,
           borderColor: [
             'rgba(255, 99, 132, 1)',
             'rgba(54, 162, 235, 1)',
@@ -111,6 +136,7 @@ const WalletInfo: NextPage = () => {
           borderWidth: 1,
         }],
     };
+    */
 
     return(
         <div className='bg-[#14161f] h-screen w-screen overflow-hidden'>
@@ -125,7 +151,7 @@ const WalletInfo: NextPage = () => {
                 }
                 {
                     tokenBalanceQuery.data && <div className='w-1/4'>
-                        <Pie data={chartData}/>
+                        {/* <Pie data={chartData}/> */}
                     </div>
                 }
             </section>
