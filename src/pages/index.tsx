69475a5bcb5e98b0c56823e8301165d22e3c2742
Shlok D. Mange <shlok_mange@protonmail.com>
2023-06-17 10:41:44 +0800

    landing page feature

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,27 @@ import { Box, Button, Stack, HStack, VStack, Text } from '@chakra-ui/react'
 const Home: NextPage = () => {
 	const header = `vybe.gg`
 
+	const features = [
+		{
+			title: `KYG - Know Your Gamers`,
+			subTitle: `: Understanding at Light Speed`,
+			content: `Stop guessing and start knowing. With our KYG protocol, we're shifting from averages to individuals, from hunches to certainties. Powered by blockchain technology, we offer a hyperloop ride into the unique world of each gamer, enabling you to craft experiences that resonate at a cosmic level.`,
+			color: `#08090c`
+		},
+		{
+			title: `Seamless Bridging`,
+			subTitle: ` Plug into the Matrix`,
+			content: `Connect the dots between the conventional and the revolutionary. Our easy-to-implement API allows Web2 games to leverage the power of blockchain, seamlessly integrating the future of tech with the platforms of today. Welcome to the symbiosis of Web2 and Web3`,
+			color: ``,
+		},
+		{
+			title: `E-Lens`,
+			subTitle: `Your Money-Lens into the Metaverse`,
+			content: `Journey through your gamers' financial universe like never before. Our cutting-edge AI navigates the nebula of transactions, offering you a detailed and insightful celestial map. It's time to launch into a new dimension of understanding your gamers' financial dynamics`,
+			color: ``
+		}
+	]
+
 	return(
 		<Box h={'100vh'} w={'100vw'} overflowX={'hidden'} overflowY={'scroll'} bgColor={'#08090c'} fontFamily={'Manrope'}>
 			<nav className='bg-[#08090c] text-gray-300 px-16 py-8 flex items-center justify-between space-x-8'>
@@ -23,6 +44,19 @@ const Home: NextPage = () => {
 					{`Dive into the minds of your gamers with Vybe.gg - your interstellar tool for player understanding in the gaming cosmos.`}
 				</Text>
 			</VStack>
+			<HStack align={'center'} justify={'space-between'} px={16} py={8}>
+				{
+					features.map(({ title, subTitle, content }, index) => {
+						return(
+							<VStack key={index}>
+								<Text>{title}</Text>
+								<Text>{subTitle}</Text>
+								<Text>{content}</Text>
+							</VStack>
+						)
+					})
+				}
+			</HStack>
 		</Box>
 	)
 }
